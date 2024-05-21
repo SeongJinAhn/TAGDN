0b7141e71ef35d4de0b166802c553ee0b299cc88
sssjjjaaa <37531907+SeongJinAhn@users.noreply.github.com>
2024-05-21 14:59:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,36 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-from src.model import HetGDC
+from src.model import TAGDN
 import argparse
-from torch_geometric.utils import negative_sampling
 from src.utils import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default='DBLP',
+    parser.add_argument('--dataset', type=str, default='ACM',
                         help='Dataset')
-    parser.add_argument('--epoch', type=int, default=300,
+    parser.add_argument('--epoch', type=int, default=50,
                         help='Training Epochs')
+    parser.add_argument('--hidden_dim', type=int, default=1024,
+                        help='Hidden dimension d_h')
     parser.add_argument('--node_dim', type=int, default=256,
-                        help='Node dimension')
+                        help='Node dimension d')
     parser.add_argument('--lr', type=float, default=0.005,
                         help='learning rate')
     parser.add_argument('--weight_decay', type=float, default=0.001,
                         help='l2 reg')  
-    parser.add_argument('--num_layers', type=int, default=15,
+    parser.add_argument('--num_layers', type=int, default=10,
                         help='number of layer')
     parser.add_argument('--device', type=str, default='cuda',
                         help='device') 
     parser.add_argument('--alpha', type=float, default=0.1,
                         help='prob of teleport')
-    parser.add_argument('--mode', type=str, default='rw',
-                        help='diffusion model [1-hop / rw / heat')
+    parser.add_argument('--t', type=float, default=3,
+                        help='diffusion time')
+    parser.add_argument('--diffusion', type=str, default='ppr',
+                        help='diffusion model [ppr / heat / gaussian]')
     parser.add_argument('--temperature', type=float, default=1)
-    parser.add_argument('--gamma', type=float, default=1)
+    parser.add_argument('--gamma', type=float, default=2)
 
     args = parser.parse_args()
     print(args)
@@ -40,6 +43,12 @@ if __name__ == '__main__':
     dev = args.device
     weight_decay = args.weight_decay
     num_layers = args.num_layers
+
+    if args.diffusion == 'ppr':
+        coeff = args.alpha
+    if args.diffusion == 'heat':
+        coeff = args.t
+
 
     if args.dataset == 'DBLP':
         features_list, adjM, labels, train_val_test_idx = load_DBLP_data()
@@ -53,10 +62,11 @@ if __name__ == '__main__':
     if args.dataset == 'FREEBASE':
         features_list, adjM, labels, train_val_test_idx = load_FREEBASE_data([20, 40, 60], [3492, 2502, 33401, 4459])
         num_relation = 6
-        
+
     N = adjM.shape[0]
     node_features = preprocess_attributes(N, args.dataset, features_list, dev)
-    edge, aa_, type_nodes = preprocess_edges(N, adjM, args.dataset, features_list, dev)
+    edge, neg_idx, type_nodes = preprocess_edges(N, adjM, args.dataset, features_list, dev)
+    neg_edge = make_negative_edge(edge, neg_idx, node_features.size()[0]).cuda()
 
     labels = torch.LongTensor(labels).to(dev)
     train_idx = train_val_test_idx['train_idx']
@@ -70,13 +80,14 @@ if __name__ == '__main__':
     final_f1 = 0
 
     node_features = node_features.to(args.device)
-    model = HetGDC(num_class=num_classes,
+    model = TAGDN(num_class=num_classes,
                         num_layers=num_layers,
                         w_in = node_features.shape[1],
+                        w_hid = args.hidden_dim,
                         w_out = node_dim,
-                        alpha=args.alpha,
+                        alpha=coeff,
                         type_nodes=type_nodes,
-                        mode=args.mode,
+                        mode=args.diffusion,
                         dataset=args.dataset,
                         temperature=args.temperature,
                         dev=args.device).to(dev)
@@ -85,31 +96,34 @@ if __name__ == '__main__':
     loss = nn.CrossEntropyLoss() 
     # Train & Valid & Test
     best_val_f1 = 0
-        
+
     for i in range(epochs):
         model.zero_grad() 
         model.train() 
-        neg_edges = negative_sampling(edge, N, edge.size()[1] * 10)
-
-        z, reg_loss = model(node_features, edge, neg_edges)
+        z = model(node_features, edge)
         loss = model.node_classification(z, train_node, labels[train_node])
-        loss += reg_loss * args.gamma
-        del neg_edges
 
         print('Epoch:  ',i+1)
-        print('Train - Loss: {}'.format(loss.detach().cpu().numpy()))
+        print('Train - Loss: {}'.format(loss.detach().cpu().numpy())) 
         loss.backward()
         optimizer.step()
         model.eval()
             
         with torch.no_grad():
+            z = model(node_features, edge, training=False)
             svm_macro_f1_list, svm_micro_f1_list, nmi_mean, nmi_std, ari_mean, ari_std = evaluate_results_nc(z[valid_node].cpu().detach().numpy(), labels[valid_node].cpu().detach().numpy(), num_classes=num_classes, mode='valid')
             val_f1 = svm_macro_f1_list[1][0]
-    
+            
+          
         if float(best_val_f1) < float(val_f1):
             best_val_f1 = val_f1
             best_z = z
 
             with torch.no_grad():
                 svm_macro_f1_list, svm_micro_f1_list, nmi_mean, nmi_std, ari_mean, ari_std = evaluate_results_nc(z[test_node].cpu().detach().numpy(), labels[test_node].cpu().detach().numpy(), num_classes=num_classes)
-svm_macro_f1_list, svm_micro_f1_list, nmi_mean, nmi_std, ari_mean, ari_std = evaluate_results_nc(best_z[test_node].cpu().detach().numpy(), labels[test_node].cpu().detach().numpy(), num_classes=num_classes)
+                z = z[:labels.size()[0]].detach().cpu().numpy()
+                kmeans = KMeans(n_clusters=len(torch.unique(labels.cpu())), random_state=42).fit(z)
+                nmi = normalized_mutual_info_score(labels.cpu(), kmeans.labels_)
+                ari = adjusted_rand_score(labels.cpu(), kmeans.labels_)
+                print(nmi, ari)
+               
