8c5b55ba4de66f12aa305af5519235016c7c9bc8
sssjjjaaa <37531907+SeongJinAhn@users.noreply.github.com>
2025-01-03 15:11:57 +0900

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 import time
+import scipy
 from ctypes.wintypes import SC_HANDLE
 from operator import neg
 from numpy.lib.type_check import real
@@ -8,12 +9,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import scipy.sparse as sp 
 from src.layers import PPR, HeatKernel, Gaussian
+from scipy.stats import wasserstein_distance
 from sklearn.metrics import roc_auc_score, average_precision_score
 
 class TAGDN(nn.Module):
-    def __init__(self, num_class, num_layers, w_in, w_out, alpha, type_nodes, mode, dataset, temperature, dev):
+    def __init__(self, num_class, num_layers, w_in, w_hid, w_out, alpha, type_nodes, mode, dataset, temperature, dev):
         super(TAGDN, self).__init__()
         self.w_in = w_in
+        self.w_hid = w_hid
         self.w_out = w_out
         self.num_layers = num_layers
         self.dataset = dataset
@@ -34,6 +37,8 @@ class TAGDN(nn.Module):
         self.ppr_diffusion = PPR(K=num_layers, alpha=alpha, renormalized=self.renormalized, symmetric=self.symmetric)
         self.heat_diffusion = HeatKernel(K=num_layers, alpha=alpha, symmetric=self.symmetric, laplacian=True)
         self.gaussian_diffusion = Gaussian(K=num_layers, alpha=alpha, laplacian=True)
+        self.node_features = nn.Parameter(torch.FloatTensor(self.w_in, self.w_out).uniform_(-1.0, 1.0).to(dev))
+        self.LP_loss = nn.BCEWithLogitsLoss(reduction='mean') 
 
         if mode == 'ppr':
             self.diffusion = self.ppr_diffusion
@@ -50,16 +55,21 @@ class TAGDN(nn.Module):
             else:
                 self.node_type += i * self.type_nodes[i]
 
-        self.type_specific_encoder = nn.Linear(w_in, w_out)
+        self.type_specific_encoder = nn.Linear(w_in, w_hid)
+        self.linear = nn.Linear(w_hid, w_out)
         self.classifier = nn.Linear(w_out, num_class)
         nn.init.xavier_normal_(self.type_specific_encoder.weight, gain=1.414)
         nn.init.xavier_normal_(self.linear.weight, gain=1.414)
         nn.init.xavier_normal_(self.classifier.weight, gain=1.414)
 
+        self.batch_norm = []
+        for i in range(self.type_nodes.size()[0]):
+            self.batch_norm.append(nn.BatchNorm1d(w_hid).cuda())
+
     def type_specific_statistics(self, H):
         for i in range(self.type_nodes.size()[0]):
-            tmp_std, tmp_mean = torch.std_mean(H[self.type_nodes[i].nonzero()],0, unbiased=False)
-            tmp_std = tmp_std * torch.sqrt(self.type_nodes[i].sum()) + 1e-9
+            tmp_mean = torch.mean(H[self.type_nodes[i].nonzero()],0)
+            tmp_std = 1 / torch.sqrt(self.type_nodes[i].sum()-1) * ((H[self.type_nodes[i].nonzero()] - tmp_mean) ** 2).sum(0)
             if i == 0:
                 mean_node_type = tmp_mean
                 std_node_type = tmp_std
@@ -76,15 +86,37 @@ class TAGDN(nn.Module):
 
     def forward(self, X, edge_index, training=True):
         # Type-Aware Encoder
-        start_time = time.time()
         H = self.type_aware_encoding(X)
-
+        
         mean_node_type, std_node_type = self.type_specific_statistics(H)
         tilde_H = (H - mean_node_type[self.node_type]) / std_node_type[self.node_type]
+        loss = self.wasserstein_loss(tilde_H)
+        tilde_H = F.dropout(tilde_H, p=0.5, training=training)
         tilde_Z = self.diffusion(tilde_H, edge_index)
         Z = tilde_Z * std_node_type[self.node_type] + mean_node_type[self.node_type]
-        Z = self.activation(Z, self.act)
-        return Z
+ 
+        Z = F.normalize(Z, p=2, dim=1)
+        return Z, loss
+    
+    def NContrast(self, z, edge_index, neg_edge, temperature=1):
+        z = F.normalize(z, p=2, dim=1)
+        pred_pos = torch.sum(z[edge_index[0]] * z[edge_index[1]], dim=1)
+        pred_neg = torch.sum(z[neg_edge[0]] * z[neg_edge[1]], dim=1)
+
+        pos = torch.exp(pred_pos/temperature).sum()
+        neg = torch.exp(pred_neg/temperature).sum()
+        del pred_pos, pred_neg
+        loss = -torch.log(pos / (pos+neg))
+        del pos, neg
+        return loss
+    
+    def wasserstein_loss(self, embeddings):
+        embeddings = embeddings.cpu().detach().numpy()
+        embeddings = embeddings.flatten()
+        gaussian_sample = np.random.normal(0, 1, embeddings.shape[0])
+        distance = wasserstein_distance(gaussian_sample, embeddings)
+        return distance
+
 
     def activation(self, z, type):
         if type == 'sigmoid':
